6d97403f97758bf3e1e68c9cafca15bba9500ef8
Marly Gotti <marlygotti@gmail.com>
2021-10-31 17:14:49 -0400

    Add identation.

--- app.R
+++ app.R
@@ -24,41 +24,43 @@ ui <- navbarPage(
     "riskmetric Survey",
     id = "inTabset",
     tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css"),
-        tabPanel("Introduction",
-                 h2("Introduction"),
-                 h5("The ",a("riskmetric package", href = "https://pharmar.github.io/riskmetric/index.html", target="_blank"), 
-                 " provides a workflow to evaluate the quality of (a set) of R packages 
-                   which involves five major steps. The workflow can help users to choose high quality 
-                   R packages, improve package reliability and prove the validity of R packages in a 
+    tabPanel("Introduction",
+             h2("Introduction"),
+             h5("The ",a("riskmetric package", href = "https://pharmar.github.io/riskmetric/index.html", target="_blank"),
+                " provides a workflow to evaluate the quality of (a set) of R packages
+                   which involves five major steps. The workflow can help users to choose high quality
+                   R packages, improve package reliability and prove the validity of R packages in a
                    regulated industry."),
-                 h5("One issue with risk is its need for context. For example, a package used for a class 
+             h5("One issue with risk is its need for context. For example, a package used for a class
                    project is of lower risk than the same package being used to determine if a new therapy is effective.
                    The risk of a package is the combination of its quality and its criticality in a user's workflow."),
-                 h5("Furthermore, weighting of package metrics (i.e. an assessment score) which make up a package's summarized risk score have
+             h5("Furthermore, weighting of package metrics (i.e. an assessment score) which make up a package's summarized risk score have
                     been done arbitrability or not at all. Enter this survey. We hope that by crowd sourcing
                    a risk rating on a number of packages will allow us to empirically determine weights for each
                    package metric used in the riskmetric package."),
-                 h2("Instructions"),
-                 h5("Please rate the packages on the next tab, based on your perceived risk of using each package in
+             h2("Instructions"),
+             h5("Please rate the packages on the next tab, based on your perceived risk of using each package in
                     your environment. Your environment could be your desktop, RStudio cloud, a company server (GxP or non-GxP), etc.
                     The survey randomly selects 15 packages from the 500-ish most downloaded packages on CRAN for you to rate. You are welcome
                     to revisit survey to rate more packages if you like, however, there is a finite chance of being asked about the same packages
                     over and over due to the anonymity of the survey."),
-                 h2("Data Collection and Privacy:"),
-                 h5("We will only collect and store the data submitted on the form. 
+             h2("Data Collection and Privacy:"),
+             h5("We will only collect and store the data submitted on the form.
                  You are free to skip any questions you are not comfortable answering.
                    Each user's submission will be uniquely identified by a random string generated when you submit the form for aggregation and modeling.
                    The riskmetric team plans to release all the collected data with the riskmetric package so that other developers
                     and users can empirically derive their own package metric weights for summarization."),
-        actionButton("jumpToP1", "Start the survey")),
-        tabPanel("Survey", value="survey",
-            uiOutput("survey")
-        )
+             actionButton("jumpToP1", "Start the survey")),
+    tabPanel("Survey", value="survey",
+             uiOutput("survey")
     )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-    
+
+    renderSurvey()
+
     output$survey <- renderUI({
         qs_idx <- sample(x = unique(qs$question)[-c(1:4)], size = 15, replace = F)
         qs_idx <- which(qs$question %in% qs_idx)
@@ -68,14 +70,12 @@ server <- function(input, output, session) {
                      with 1 being low risk and 5 being high risk. If you are unfamiliar with
                      the package you may mark NA. All responses are anonymous.")
     })
-    
+
     observeEvent(input$jumpToP1, {
         updateTabsetPanel(session, "inTabset",
                           selected = "survey")
     })
-    
-    renderSurvey()
-    
+
     observeEvent(input$submit, {
         response_data <- getSurveyData()
         response_data$subject_id <- gsub("/", "", tempfile(pattern = "user", tmpdir = ""))
@@ -88,5 +88,5 @@ server <- function(input, output, session) {
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
